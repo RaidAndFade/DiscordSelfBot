2a0a30007d344161ff608d45d3582ed789a682cb
memanlolz <memanlolz@users.noreply.github.com>
2016-08-20 18:35:31 -0400

    Base added to weather.js
    Weather base file has been prepared with all events and such commented out in case of the desire to use them.

--- Modules/weather.js
+++ Modules/weather.js
@@ -1,1 +1,24 @@
 
+const EventEmitter = require('events');
+class EE extends EventEmitter{}
+const weather = new EE();
+weather.name="Weather";
+
+//weather.on("load",(p)=>{});
+//weather.on("message",(p,user,channelId,message,rawEvent)=>{console.log(user.id+" said "+rawEvent.d.id)});
+//weather.on("message_updated",(p,msgId,user,channelId,message,rawEvent)=>{console.log(user.id+" edited "+msgId)});
+//weather.on("message_deleted",(p,msgId,user,channelId,message,rawEvent)=>{console.log(user.id+" deleted "+msgId)});
+
+weather.commands = {
+	weather: {
+		parse: utils.combinate.self.seq(utils.combine.vars.all),	
+		usage: "weather",
+		desc: "Get the weather of an area",
+		run: (p, args, user, channel, event) => {
+		  //The city is given in args[0].
+			//p.reply(event, user.tag+"you can reply to commands like this")
+		}
+	}
+}
+
+module.exports=weather;
